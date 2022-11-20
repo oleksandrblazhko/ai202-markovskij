0a3f7133d36784d1333282101d42e3ef0d8d29bc
Danilo Markovskij <di0niss@ukr.net>
2022-11-20 15:52:32 +0200

    Added all file on the branch

---
+++ 3-SoftwareConstruction/4-Program-Methods-Example.cs
@@ -0,0 +1,51 @@
+﻿public class LoginDataServices
+{
+    private readonly IMongoCollection<LoginData> _loginData;
+    private const string Connection =
+        "mongodb+srv://profile:test1@cluster0.6wbdzra.mongodb.net/?retryWrites=true&w=majority";
+
+    private const string DatabaseName =
+        "application_db";
+
+
+    public LoginDataServices()
+    {
+        var mongoClient = new MongoClient(Connection);
+
+        _loginData = mongoClient
+            .GetDatabase(DatabaseName)
+            .GetCollection<LoginData>("user_login_data");
+    }
+
+    public async Task Create(LoginData newLoginData) =>
+        await _loginData.InsertOneAsync(newLoginData);
+
+    public async Task<List<LoginData>> GetAll() =>
+        await _loginData.Find(_ => true).ToListAsync();
+
+    public async Task<int> LoginIn(string login, string password)
+    {
+
+        if (login.Length < 6 || login.Length > 32)
+            return -1;
+
+        if (password.Length < 8 || password.Length > 24)
+            return -2;
+
+        LoginData userDataInDatabase;
+        try
+        {
+            userDataInDatabase =
+                       await _loginData.Find(c => c.Login == login).FirstAsync();
+            
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+            Console.WriteLine($"Користувача {login} не iснує!");
+            return -3;
+        }
+        return 0;
+    }
+
+}
---
+++ 3-SoftwareConstruction/5-Program-Methods-Using-Example.cs
@@ -0,0 +1,40 @@
+﻿LoginDataServices db = new LoginDataServices();
+
+LoginData newData = new LoginData()
+{
+    Login = "Mojahed",
+    Email = "test@gmail.com",
+    Password = "12345678"
+};
+
+var list = await db.GetAll();
+
+foreach(var item in list)
+{
+    var json = new JavaScriptSerializer().Serialize(item);
+    Console.WriteLine(json);
+}
+
+//Code -1
+string login = "Moj";
+string password = "12345678";
+
+var account = await db.LoginIn(login, password);
+
+Console.WriteLine(account);
+
+//Code -2
+login = "Mojahed";
+password = "123";
+
+var account2 = await db.LoginIn(login, password);
+
+Console.WriteLine(account2);
+
+//Code -3
+login = "Mojahed21";
+password = "12345678";
+
+var account3 = await db.LoginIn(login, password);
+
+Console.WriteLine(account3);
