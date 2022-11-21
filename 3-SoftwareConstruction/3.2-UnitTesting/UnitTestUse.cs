90e03fbc5581d22eae051b2cd5a8962947bf691f
Danilo Markovskij <di0niss@ukr.net>
2022-11-22 01:29:35 +0200

    Added Use of Unit Test

---
+++ 3-SoftwareConstruction/3.2-UnitTesting/UnitTestUse.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        LoginDataServices db = new LoginDataServices();
+
+
+        string login = new string('t', 40);
+        string password = "12345678";
+
+        Console.WriteLine($"\nLogin: {login}; password: {password}");
+        UnitTest.LoginTest(login, password);
+
+        login = "Mojahed";
+        password = null;
+
+        Console.WriteLine($"\nLogin: {login}; password: {password}");
+        UnitTest.LoginTest(login, password);
+
+        login = "Mojahed";
+        password = "12345678";
+
+        Console.WriteLine($"\nLogin: {login}; password: {password}");
+        UnitTest.LoginTest(login, password);
+    }
+}
